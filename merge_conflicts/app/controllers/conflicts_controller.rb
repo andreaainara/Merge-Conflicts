d7b8abd48bec393061f77b7d1428676bd96b5b1a
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-05-05 10:21:50 -0700

    fixed conflic

--- merge_conflicts/app/controllers/conflicts_controller.rb
+++ merge_conflicts/app/controllers/conflicts_controller.rb
@@ -1,5 +1,5 @@
 class ConflictsController < ApplicationController
-<<<<<<< HEAD
+
 
 
   def index
@@ -8,8 +8,7 @@ class ConflictsController < ApplicationController
 
   def most
     print hello
-=======
-  def most
->>>>>>> 117a609650fdf87261ad16daeb887fe7962cdcc4
   end
+
+
 end
